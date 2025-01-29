7bad3e7891bc3b412852f95d9d9ea35a293d0166
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-01-29 17:14:32 +0200

    Add pages for sign up and sign in

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,37 @@ import './index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App.jsx';
+import {
+  SIGNUP_PATH,
+  SIGNIN_PATH,
+  authFormAction,
+} from './components/AuthForm/auth-form-utils';
+import AuthForm from './components/AuthForm/AuthForm';
+import Guard from './components/AuthGuard/AuthGuard';
+import App from './App';
 
-const router = createBrowserRouter([{ path: '/', element: <App /> }]);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: SIGNUP_PATH,
+        element: <AuthForm key={`${SIGNUP_PATH.replace('/', '')}-form`} />,
+        action: authFormAction,
+      },
+      {
+        path: SIGNIN_PATH,
+        element: <AuthForm key={`${SIGNIN_PATH.replace('/', '')}-form`} />,
+        action: authFormAction,
+      },
+      {
+        element: <Guard authPath={SIGNIN_PATH} />,
+        children: [{ index: true, element: <h2 className="mt-16">Home</h2> }],
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
