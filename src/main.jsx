36a70ee0197c2d66520c11526a6128d8edf3db05
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-02-02 15:04:12 +0200

    Delegate adding the page's title tag to the router

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { authFormAction } from './components/AuthForm/auth-form-utils';
+import App, { SIGNUP_PATH, SIGNIN_PATH, SIGNOUT_PATH } from './App';
 import AuthForm from './components/AuthForm/AuthForm';
 import Guard from './components/AuthGuard/AuthGuard';
-import App, { SIGNUP_PATH, SIGNIN_PATH, SIGNOUT_PATH } from './App';
+import PageTitle from './PageTitle';
 
 const router = createBrowserRouter([
   {
@@ -14,19 +15,45 @@ const router = createBrowserRouter([
     children: [
       {
         path: SIGNUP_PATH,
-        element: <AuthForm key={`${SIGNUP_PATH.replace('/', '')}-form`} />,
+        element: (
+          <>
+            <PageTitle pageTitle="Sign up" />
+            <AuthForm key="signup-form" />
+          </>
+        ),
         action: authFormAction,
       },
       {
         path: SIGNIN_PATH,
-        element: <AuthForm key={`${SIGNIN_PATH.replace('/', '')}-form`} />,
+        element: (
+          <>
+            <PageTitle pageTitle="Sign in" />
+            <AuthForm key="signin-form" />
+          </>
+        ),
         action: authFormAction,
       },
       {
         element: <Guard authPath={SIGNIN_PATH} />,
         children: [
-          { path: SIGNOUT_PATH, element: <App /> },
-          { index: true, element: <h2 className="mt-16">Home</h2> },
+          {
+            path: SIGNOUT_PATH,
+            element: (
+              <>
+                <PageTitle pageTitle="Sign out" />
+                <App />
+              </>
+            ),
+          },
+          {
+            index: true,
+            element: (
+              <>
+                <PageTitle pageTitle="Home" />
+                <h2 className="mt-16">Home</h2>
+              </>
+            ),
+          },
         ],
       },
     ],
