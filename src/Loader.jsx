ed1e9b2a285d196ab2dd0786d005d37de2e39caf
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-02-09 14:50:43 +0200

    Edit styles of Loader component

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -1,14 +1,18 @@
+import PropTypes from 'prop-types';
 import { BiLoaderAlt } from 'react-icons/bi';
 
-function Loader() {
+function Loader({ className, ...props }) {
   return (
-    <span
+    <div
       aria-label="Loading..."
-      className="animate-spin text-4xl text-app-main"
+      className={`w-fit animate-spin text-4xl text-app-main mx-auto${className ? ' ' + className : ''}`}
+      {...props}
     >
       <BiLoaderAlt />
-    </span>
+    </div>
   );
 }
+
+Loader.propTypes = { className: PropTypes.string };
 
 export default Loader;
