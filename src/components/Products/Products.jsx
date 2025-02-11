44470b2178a922f06dc0a36bc91eff033cca99d3
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-02-11 14:17:17 +0200

    Center the product cards

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,7 +3,7 @@ import ProductCard, { ItemType } from '../ProductCard/ProductCard';
 
 function Products({ items }) {
   return (
-    <div className="container mx-auto flex flex-wrap w-full px-2">
+    <div className="container w-full px-2 mx-auto flex flex-wrap justify-center">
       {items.map((item) => (
         <ProductCard key={item.product?.id || item.id} item={item} />
       ))}
